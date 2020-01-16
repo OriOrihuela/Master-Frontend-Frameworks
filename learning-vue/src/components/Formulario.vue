611693880432144fcdd4c21313f61aec7627ecdf
Eduardo Orihuela Verdugo <Eduardo.orihuela@sm2baleares>
2020-01-16 10:41:05 +0100

    feat: create a form and link its values to a JS object
    Create a form and link the introduced values by the user into a JS
    object.
    
    BREAKING CHANGE: !
    Closes !

--- learning-vue/src/components/Formulario.vue
+++ learning-vue/src/components/Formulario.vue
@@ -3,6 +3,31 @@
     <div class="center">
       <section id="content">
         <h2 class="subheader">Formulario</h2>
+        <form action="#" class="mid-form" @submit.prevent="mostrarUsuario">
+          <div class="form-group">
+            <label for="nombre">Nombre</label>
+            <input type="text" name="nombre" v-model="user.nombre" />
+          </div>
+          <div class="form-group">
+            <label for="apellidos">Apellidos</label>
+            <input type="text" name="apellidos" v-model="user.apellidos" />
+          </div>
+          <div class="form-group">
+            <label for="bio">Biografia</label>
+            <textarea name="bio" cols="30" rows="10" v-model="user.bio"></textarea>
+          </div>
+          <div class="form-group radibuttons">
+            <input type="radio" name="genero" value="hombre" checked v-model="user.genero" /> Hombre
+            <input type="radio" name="mujer" v-model="user.genero" /> Mujer
+            <input type="radio" name="otro" v-model="user.genero" /> Otro
+          </div>
+
+          <!-- LIMPIAR FLOATS -->
+          <div class="clearfix"></div>
+
+          <!-- SUBMIT -->
+          <input type="submit" value="Enviar" class="btn btn-success" />
+        </form>
       </section>
       <Sidebar></Sidebar>
       <!-- LIMPIAR FLOATS -->
@@ -18,6 +43,22 @@ export default {
   name: "Formulario",
   components: {
     Sidebar
+  },
+  data() {
+    return {
+      user: {
+        nombre: "",
+        apellidos: "",
+        bio: "",
+        genero: ""
+      }
+    };
+  },
+  // Own methods.
+  methods: {
+    mostrarUsuario() {
+      console.log(this.user);
+    }
   }
 };
 </script>
